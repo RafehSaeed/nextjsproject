0ee1a07c611bf7cd5b78dcf368b2abcf47a997f6
rafehsaeed <rafeh_saeed@hotmail.ca>
2019-03-13 21:04:29 -0400

    styling registration page

--- pages/register.js
+++ pages/register.js
@@ -2,11 +2,9 @@ import Layout from '../components/nav/Layout'
 import { Form, Input, TextArea, Button, Select, Rating } from 'semantic-ui-react'
 
 export default class RegisterPage extends React.Component {
+
   static async getInitialProps({query}) {
-
-	return { query }
-
-
+  	return { query }
   }
 
 
@@ -30,16 +28,17 @@ export default class RegisterPage extends React.Component {
 
   render() {
     const userTypes =  [ //options for drop down select
+					{ key: 'select', text: 'Select', value: '' },
 					{ key: 'reg', text: 'Regular User', value: 'reg' },
 					{ key: 'sp', text: 'Service Provider', value: 'sp' },
 				]
-
 	return (
 		<Layout>
 			<div className="container">
 				<div className="view view-categories">
-					<div className="view-wrapper">
+					<div className="view-wrapper form-fields">
 						<Form>
+							<h2 className="registration-title">Register <span>now</span></h2>
 							<Form.Field label='First Name' id="first_name" name="first_name" placeholder="First Name" control='input' type='text' />
 							<Form.Field label='Last Name' id="last_name" name="last_name" placeholder="Last Name" control='input' type='text' />
 							<Form.Field label='Email Address' id="email_addr" name="email_addr" placeholder="Email " control='input' type='text' />
